2bed78469c43a025aaa9171494fe600308503085
pratik-bagul <pratik.bagul@wolterskluwer.com>
2026-01-20 14:59:47 +0530

    initial symfony todo project setup

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Form\TaskType;
+use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+/**
+ * Teaching notes (read along):
+ * - We use PHP 8 attributes for routing (#[Route(...)]). Each method maps to a URL + HTTP method.
+ * - We inject repositories and the EntityManager to talk to the database.
+ * - We return either HTML (Twig templates) or JSON (for APIs).
+ */
+#[Route('/tasks')]
+class TaskController extends AbstractController
+{
+    /**
+     * LIST: GET /tasks
+     * Shows all tasks (HTML) and returns JSON if requested via Accept: application/json.
+     */
+    #[Route('', name: 'task_index', methods: ['GET'])]
+    public function index(TaskRepository $repo, Request $request): Response
+    {
+        $tasks = $repo->findBy([], ['dueAt' => 'ASC']);
+
+        // If client asks for JSON, return JSON (simple content negotiation)
+        if ($request->headers->get('Accept') === 'application/json') {
+            return $this->json($tasks, 200, [], ['groups' => ['task:list']]);
+        }
+
+        return $this->render('task/index.html.twig', [
+            'tasks' => $tasks,
+        ]);
+    }
+
+    /**
+     * CREATE (Form): GET+POST /tasks/new
+     * Renders a form and handles submission.
+     */
+    #[Route('/new', name: 'task_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($task);
+            $em->flush();
+
+            $this->addFlash('success', 'Task created!');
+            return $this->redirectToRoute('task_index');
+        }
+
+        return $this->render('task/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * SHOW: GET /tasks/{id}
+     * Automatic ParamConverter fetches Task by id.
+     */
+    #[Route('/{id}', name: 'task_show', methods: ['GET'])]
+    public function show(Task $task, Request $request): Response
+    {
+        if ($request->headers->get('Accept') === 'application/json') {
+            return $this->json($task, 200, [], ['groups' => ['task:detail']]);
+        }
+
+        return $this->render('task/show.html.twig', [
+            'task' => $task,
+        ]);
+    }
+
+    /**
+     * EDIT (Form): GET+POST /tasks/{id}/edit
+     */
+    #[Route('/{id}/edit', name: 'task_edit', methods: ['GET', 'POST'])]
+    public function edit(Task $task, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'Task updated!');
+            return $this->redirectToRoute('task_index');
+        }
+
+        return $this->render('task/edit.html.twig', [
+            'task' => $task,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * DELETE (CSRF-protected): POST /tasks/{id}
+     * Submit from HTML form with hidden _method=DELETE or POST route dedicated to delete.
+     */
+    #[Route('/{id}', name: 'task_delete', methods: ['POST'])]
+    public function delete(Task $task, Request $request, EntityManagerInterface $em): Response
+    {
+        // CSRF token should be generated in Twig: {{ csrf_token('delete_task_' ~ task.id) }}
+        if ($this->isCsrfTokenValid('delete_task_' . $task->getId(), $request->request->get('_token'))) {
+            $em->remove($task);
+            $em->flush();
+            $this->addFlash('info', 'Task deleted.');
+        } else {
+            $this->addFlash('error', 'Invalid CSRF token.');
+        }
+
+        return $this->redirectToRoute('task_index');
+    }
+
+    /**
+     * (Optional) QUICK TOGGLE: POST /tasks/{id}/toggle
+     * Handy endpoint to mark done/undone without opening the edit form.
+     */
+    #[Route('/{id}/toggle', name: 'task_toggle', methods: ['POST'])]
+    public function toggle(Task $task, EntityManagerInterface $em): Response
+    {
+        $task->setIsDone(!$task->isDone());
+        $em->flush();
+        return $this->redirectToRoute('task_index');
+    }
+}
---
+++ src/Entity/Task.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TaskRepository::class)]
+class Task
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $isDone = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $dueAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function isDone(): ?bool
+    {
+        return $this->isDone;
+    }
+
+    public function setIsDone(?bool $isDone): static
+    {
+        $this->isDone = $isDone;
+
+        return $this;
+    }
+
+    public function getDueAt(): ?\DateTimeImmutable
+    {
+        return $this->dueAt;
+    }
+
+    public function setDueAt(?\DateTimeImmutable $dueAt): static
+    {
+        $this->dueAt = $dueAt;
+
+        return $this;
+    }
+}
